68b37c3401eb0e0eb5e1768bee648f8d24b28956
gungunxotiv-source <gungun.xotiv@gmail.com>
2025-12-19 16:48:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <p>App</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import "./Navbar.css"
+ const Navbar = () => {
+  return  <p>tewt</p> 
+  (
+    <div>
+        {/* <nav>
+            <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact us</li>
+             <li>Contact us</li>
+            </ul>
+        </nav> */}
+        <p style={{color: "black"}}>tewt</p>
+    </div>
+  )
+}
+
+export default Navbar
